b118fe3d7e1c3f6229ee006b5bc5c91d1da0ee16
benjsant <santrisseb@gmail.com>
2025-07-10 16:53:56 +0200

    historique coach finis plus que les toast

--- seances/urls.py
+++ seances/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
      path("annuler/<int:seance_id>/", views.annuler_seance, name="annuler_seance"),
      path("rdv/<int:rdv_id>/fin/", views.confirmer_fin_rdv, name="confirmer_fin_rdv"),
      path("absent/<int:seance_id>/", views.marquer_absent, name="marquer_absent"),
-     path("histo_client/", views.historique_client , name="histo_client"),
-     path("histo_coach/", views.historique_coach , name="histo_coach"),
+     path("historique_client/", views.historique_client , name="historique_client"),
+     path("historique_coach/", views.historique_coach , name="historique_coach"),
      path('futures_sessions/', views.futures_sessions_coach, name='futures_sessions_coach'),
 ]
