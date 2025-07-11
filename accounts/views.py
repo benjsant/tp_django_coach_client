77cf12b600f99003c73039aa03a2a0093a140a09
benjsant <santrisseb@gmail.com>
2025-07-11 12:06:33 +0200

    Feature model (#3)
    * model cree, modif css, modif login signup prise_rdv_cree et amelioration graphique
    
    * quasi dernier ligne droite
    
    * ajustement quasi terminé
    
    * historique coach finis plus que les toast
    
    * rush final

--- accounts/views.py
+++ accounts/views.py
@@ -5,28 +5,34 @@ from django.contrib import messages
 from .forms import CustomLoginForm, CustomSignupForm
 from django.contrib.auth.models import Group
 from django.contrib.auth.decorators import login_required 
-# Create your views here.
+from django.views.decorators.cache import never_cache
+from django.utils.timezone import now, localtime,localdate
+from seances.models import Seance
+from datetime import date
 
+@never_cache
 def login_user(request):
     if request.method == "POST":
         form = CustomLoginForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-
             return redirect("accounts:dashboard")
         else:
             messages.error(request, "Identifiant ou mot de passe incorrect.")
     else:
         form = CustomLoginForm()
 
-    return render(request, "accounts/login.html", {"form": form})
+    return render(request, "accounts/login.html", {
+        "form": form,
+    })
 
 def logout_user(request):
     logout(request)
     messages.success(request, "Vous avez été déconnecté avec succès.")
     return redirect("core:index")
 
+@never_cache
 def signup_user(request):
     if request.method == "POST":
         form = CustomSignupForm(request.POST)
@@ -34,17 +40,21 @@ def signup_user(request):
             user = form.save()
             client_group, created = Group.objects.get_or_create(name="client")
             client_group.user_set.add(user)
-            login(request,user)  # connexion automatique
-            request.session['show_signup_modal'] = True  # flag pour modal
-            return redirect("accounts:dashboard")  # redirige vers dashboard
+            login(request,user)
+            request.session['show_signup_modal'] = True
+            return redirect("accounts:dashboard")
         else:
             messages.error(request,"Une erreur est survenue. Veuillez corriger les champs .")
-    else: 
+    else:
         form = CustomSignupForm()
 
-    return render(request, "accounts/signup.html",{"form":form})
+    return render(request, "accounts/signup.html", {
+        "form": form,
+    })
+
 
 @login_required
+@never_cache
 def dashboard(request):
     if request.user.groups.filter(name="coach").exists():
         return redirect("accounts:dashboard_coach")
@@ -52,26 +62,49 @@ def dashboard(request):
         return redirect("accounts:dashboard_client")
     else: 
         return redirect("core:index")
-    
+
+
 @login_required
+@never_cache
 def dashboard_client(request):
     # Récupérer et supprimer la clé 'show_signup_modal' de la session
     show_signup_modal = request.session.pop('show_signup_modal', None)
+
+    current_datetime = localtime(now())  # datetime aware au fuseau local
+
+    mes_rendezvous = Seance.objects.filter(
+        client=request.user
+    ).filter(
+        date__gt=current_datetime.date()
+    ) | Seance.objects.filter(
+        client=request.user,
+        date=current_datetime.date(),
+        heure_debut__gt=current_datetime.time()
+    )
+    mes_rendezvous = mes_rendezvous.order_by('date', 'heure_debut')
+
     context = {
         'is_client': True,
         'is_coach': False,
-        'show_signup_modal': show_signup_modal,  # Passe ça au template,
-        }
+        'show_signup_modal': show_signup_modal,
+        'mes_rendezvous': mes_rendezvous,
+    }
     return render(request, 'accounts/dashboard_client.html', context)
 
 @login_required
+@never_cache
 def dashboard_coach(request):
-    # Récupérer et supprimer la clé 'show_signup_modal' de la session
-    show_signup_modal = request.session.pop('show_signup_modal', None)
+    today = localdate()  # Date du jour, timezone-aware
+
+    # Ne récupérer que les séances du jour
+    rendezvous_coach = Seance.objects.filter(
+        coach=request.user,
+        date=today
+    ).order_by('heure_debut')
+
     context = {
         'is_client': False,
         'is_coach': True,
+        'rendezvous_coach': rendezvous_coach,
     }
     return render(request, 'accounts/dashboard_coach.html', context)
-    # rendez_vous = RendezVous.objects.filter(coach=request.user)
-    # return render(request, 'dashboard_coach.html', {'rendez_vous': rendez_vous})
--- seances/urls.py
+++ seances/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
 from . import views
-
+# path("futur_coach/", views.historique_client , name="histo_client")
 app_name = "seances"
-# urlpatterns = [
-#     path("", views.index, name="index"),
-# ]
+urlpatterns = [
+     path("prise_rdv/", views.prise_rdv, name="prise_rdv"),
+     path("annuler/<int:seance_id>/", views.annuler_seance, name="annuler_seance"),
+     path("rdv/<int:rdv_id>/fin/", views.confirmer_fin_rdv, name="confirmer_fin_rdv"),
+     path("absent/<int:seance_id>/", views.marquer_absent, name="marquer_absent"),
+     path("historique_client/", views.historique_client , name="historique_client"),
+     path("historique_coach/", views.historique_coach , name="historique_coach"),
+     path('futures_sessions/', views.futures_sessions_coach, name='futures_sessions_coach'),
+]
